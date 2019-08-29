cad8c94bd8e36ef2b9b7b00a206d8d71f2de09a6
Dhawal Seth <sethd@sethd-a01.vmware.com>
2019-08-29 10:09:45 -0700

    Adding support to import submodules

--- in/models.go
+++ in/models.go
@@ -7,9 +7,14 @@ import (
 type Request struct {
 	Source  resource.Source  `json:"source"`
 	Version resource.Version `json:"version"`
+	Params Params            `json:"params"`
 }
 
 type Response struct {
 	Version  resource.Version  `json:"version"`
 	Metadata resource.Metadata `json:"metadata"`
+}
+
+type Params struct {
+	Submodules string   `json:"submodules"`
 }
